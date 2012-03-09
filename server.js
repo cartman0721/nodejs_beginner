e49675418f504896c97112bef6f1ba999df993d7
unknown <sundazuo@gmail.com>
2012-03-09 22:17:36 +0800

    test

--- server.js
+++ server.js
@@ -3,23 +3,28 @@ var url = require("url");
 
 function start(route, handle) {
   function onRequest(request, response) {
-    var postData = "";
-    var pathname = url.parse(request.url).pathname;
-    console.log("Request for " + pathname + " received.");
-
-    request.setEncoding("utf8");
-
-    request.addListener("data", function(postDataChunk) {
-      postData += postDataChunk;
-      console.log("Received POST data chunk '"+
-      postDataChunk + "'.");
+  if (req.url == '/upload' && req.method.toLowerCase() == 'post') {
+    // parse a file upload
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files) {
+      res.writeHead(200, {'content-type': 'text/plain'});
+      res.write('received upload:\n\n');
+      res.end(sys.inspect({fields: fields, files: files}));
     });
-
-    request.addListener("end", function() {
-      route(handle, pathname, response, postData);
-    });
-
+    return;
   }
+
+  // show a file upload form
+  res.writeHead(200, {'content-type': 'text/html'});
+  res.end(
+    '<form action="/upload" enctype="multipart/form-data" '+
+    'method="post">'+
+    '<input type="text" name="title"><br>'+
+    '<input type="file" name="upload" multiple="multiple"><br>'+
+    '<input type="submit" value="Upload">'+
+    '</form>'
+  );
+}
 
   http.createServer(onRequest).listen(8888);
   console.log("Server has started.");
